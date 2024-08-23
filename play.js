f26ed8262ac49b9d3e640b678e42793a620a355b
avgamecompany <originalavgamecompany@gmail.com>
2024-08-23 20:08:03 +0300

    Add files via upload

---
+++ play.js
@@ -0,0 +1,22 @@
+function prime(x) {
+
+    var d= document.createElement("div");
+
+    d.innerText =x;
+
+    document.getElementById("x").append(d);
+}
+
+prime("all what you see here is in a video the link is in the end of the page");
+prime("let's start :)");
+prime("first run command prompt as administrator and type powercfg.exe /hibernate off and click enter ");
+prime("after that close the app and open registry editor and go to:HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Power find the hibernateEnabeld option double click on it here change the value from 1 to 0 prees ok and leave the app");
+prime("now restart your pc in bios in the bios choose secure boot and go to secure boot enable and disabele it and click yes nd save and exit the bios. ");
+prime("now open powerShell type wsl --instal and enter click yes and yes again now when its done installing restart your pc after you restart you will see a pop-up screen it will ask you to do a user name and password when you do the pass word it looks like its not typing but it is typing");
+prime("now run in the ubuntu window type sudo apt update && sudo apt -y install pv cgpt when its done installing close ubuntu and run control panel and search create a partition open it here right click c and click shrink volume now enter the amount of the srink that is 100000 and click srhink now you will see a new block right click it  and click new simpale volume and click next until you see volume label callit ChromeOS next and finish close the app");
+prime("now open the file explorer go to this pc and chrome os here create a new folder named brunch and close it   ");
+prime("now go to your web browser and type that url:https://github.com/sebanc/brunch/releasesr find assets and download the file the ends with tar.gz click on it and it will download the brunch file now in the adress bar type cros.tech  now check what is you cpu for the site for intel 6th gen to 9th gen the code name for the site is shyvana for 10th gen its Jinlon for 11th gen and newer its voxel and for and for ryzen is gumboz when you got it type it in the search bar and download the latest version now go to https://www.majorgeeks.com/files/details/grub2win.html and click on download now extract all the zips and the tar.gz now create a new folder and call it  chromeos move all of the folders all of the files that is in the folders move them to the chromeos folder   ");
+prime("rename recovery file to chromeos and copy the chromeos folder path from the adress bar and work on this command cd /mnt/your chromeos folder location when you done with the command paste it with right click in ubuntu now work on that command and dont close ubuntu sudo bash chromeos-install.sh -src chromeos.bin -dst /mnt/drive letter/brunch/chromeos.img -s drive size that is 2 gb less that the real size. when you done  paste it in ubuntu");
+prime("it will ask you if you want a single install or dualboot type dualboot and enter nnow close ubuntu when its done after that go to the chromeos partition you created and brunch open the grub text file and copy all the text in the file  then close it go to downloads and to the grub2win folder and the g2winstall if you get an errors or warnings just click ok  after its installed click on manege boot menu create a new entity in the type section choose create user section it will open note pad and you need to past it and save it and close the notepad close all the warnings hit apply then hit close now restart you pc in bios again go to the boot or startup section there make sure that its grub2win then save and exit.  ");
+prime("now evry time you will boot your pc it will boot into grub2win now search for bruch and click enter on it  in the first time you will see the brunch logo and then you will see the chrome os logo now log in with your google accuont after the setup you will be in the chromeos main screen ");
+prime("thanks for readding this website");
